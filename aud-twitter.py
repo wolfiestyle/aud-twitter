1c989764585f4be3e0c1d1b24f2c98647075cf47
darkstalker <slayerbeast@gmail.com>
2012-03-24 23:44:42 -0300

    append command line arguments to end of tweet message

--- aud-twitter.py
+++ aud-twitter.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 #
 # aud-twitter: Publish on twitter what you are listening on Audacious
 # based on mpd-twitter by Zerial
 #
 
-import os, subprocess, ConfigParser
+import os, subprocess, ConfigParser, sys
 from twitter import Api
 
 config = ConfigParser.ConfigParser()
@@ -17,9 +17,12 @@ ACCESS_TOKEN_SECRET = config.get('connect_params', 'ACCESS_TOKEN_SECRET', 1)
 
 TWEET_BEFORE = config.get('tweet_params', 'BEFORE', 1)
 TWEET_AFTER = config.get('tweet_params', 'AFTER', 1)
+TWEET_EXTRA = sys.argv[1:]
 
 curr_song = subprocess.Popen(['audtool', 'current-song'], stdout=subprocess.PIPE)
 status = TWEET_BEFORE + ' ' + curr_song.stdout.read().rstrip() + ' ' + TWEET_AFTER
+if len(TWEET_EXTRA):
+    status += '// ' + ' '.join(TWEET_EXTRA)
 
 api = Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token_key=ACCESS_TOKEN_KEY, access_token_secret=ACCESS_TOKEN_SECRET)
 new_status = api.PostUpdate(status)
